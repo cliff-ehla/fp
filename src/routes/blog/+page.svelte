ff92bb90e77af0a012c8b6afa41e84106fca2dd4
cliff <cliff.wong@ehlacademy.org>
2022-12-06 10:22:17 +0800

    link to detail page

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,8 +1,9 @@
 <script>
 	import {onMount} from "svelte";
+	import dayjs from 'dayjs'
 	let posts
 	onMount(async () => {
-		let res = await fetch("http://localhost:1337/api/posts", {
+		let res = await fetch("http://localhost:1337/api/posts?sort=createdAt:desc&populate=%2A", {
 			headers: {
 				'Authorization': "bearer 62f46ad28f35dfce1e68b04a87d5448d3177ab599b49de295308bcc59bda1079413583412b9604869a0fdf202d63416dce4f0d3cb908fd8e45724746c22fbdf199dc66f8601085cf48fa779ce8fd6abe6cd391717a3db3b7f6c71b45eb969106aa211689eaa2d2cd29667d68f5ecce638c133f2933193b35ef27f69c2759668b"
 			}
@@ -13,10 +14,24 @@
 	})
 </script>
 
+<div class="p-4">
 {#if posts}
 	{#each posts as p}
-		<p><b>{p.attributes.title}</b></p>
-		<p>{p.attributes.wp_large_url}</p>
-		<img src="http://localhost:8888/wp-content/uploads/{p.attributes.wp_large_url}" alt="">
+		<a href="/blog/{p.id}" class="block mb-8">
+			<img src="http://localhost:8888/wp-content/uploads/{p.attributes.wp_large_url}" alt="">
+			<p><b>{p.attributes.title}</b></p>
+			<p>{dayjs(p.attributes.createdAt).format('YYYY-MM-DD')}</p>
+			<div class="flex flex-wrap">
+				{#each p.attributes.tags.data as t}
+					<p class="text-xs bg-gray-300 mr-2 mb-1">{t.attributes.name}</p>
+				{/each}
+			</div>
+			<div class="flex flex-wrap">
+				{#each p.attributes.categories.data as t}
+					<p class="text-xs bg-red-300 mr-2 mb-1">{t.attributes.name}</p>
+				{/each}
+			</div>
+		</a>
 	{/each}
 {/if}
+</div>
--- src/routes/blog/[id]/+page.svelte
+++ src/routes/blog/[id]/+page.svelte
@@ -1,30 +1,22 @@
 <script>
 	import {onMount} from "svelte";
+	import {page} from "$app/stores";
 	let post
-	let image_link
 	onMount(async () => {
-		// await getTagsFromWP()
-		// await getCategoriesFromWP()
-		let res = await fetch("http://localhost:8888/wp-json/wp/v2/posts/15802")
+		let res = await fetch(`http://localhost:1337/api/posts/${$page.params.id}`, {
+			headers: {
+				'Authorization': "bearer 62f46ad28f35dfce1e68b04a87d5448d3177ab599b49de295308bcc59bda1079413583412b9604869a0fdf202d63416dce4f0d3cb908fd8e45724746c22fbdf199dc66f8601085cf48fa779ce8fd6abe6cd391717a3db3b7f6c71b45eb969106aa211689eaa2d2cd29667d68f5ecce638c133f2933193b35ef27f69c2759668b"
+			}
+		})
 		let json = await res.json()
-		post = json
-		const image_id = json.feature_image
-		let res2 = await fetch(`http://localhost:8888/wp-json/wp/v2/media/${image_id}`)
-		let json2 = await res2.json()
-		image_link = json2.source_url
-		// image_link = json2.media_details.image_meta.sizes.medium.source_url
-		// console.log(json2)
+		post = json.data.attributes
 	})
 </script>
 
 {#if post}
-	<p>{post.date}</p>
-	<p>by {post.author}</p>
-	<p>image {post.featured_media}</p>
-	{#if image_link}
-		<p>{image_link}</p>
-		<img src={image_link} alt="">
-	{/if}
-	{@html post.title.rendered}
-	{@html post.content.rendered}
+	<div class="p-4">
+		<p>{post.title}</p>
+		<img src="http://localhost:8888/wp-content/uploads/{post.wp_large_url}" alt="">
+		<p>{@html post.content}</p>
+	</div>
 {/if}
