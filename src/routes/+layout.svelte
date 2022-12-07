43e8e462c75ee2357009a3bd4a914aa75238a2d4
cliff <cliff.wong@ehlacademy.org>
2022-12-07 16:24:28 +0800

    add logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	import '../style/tailwind.css'
 </script>
 <div class="flex px-4 items-center border-b border-gray-400 h-12">
-	<a href="{base}" class="font-bold mr-4">Floating Project 3</a>
+	<a href="/{base}" class="font-bold mr-4">Floating Project 3</a>
 	<a href="{base}/art-notes/page/1">Art notes</a>
 	<a href="{base}/event">Events</a>
 	<a href="{base}/collective">Collective</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,13 @@
-<h1>Welcome to Floating Projects</h1>
+<script>
+	import {fly} from 'svelte/transition'
+	import {onMount} from "svelte";
+	let show_logo = false
+	onMount(() => {
+		setTimeout(() => {
+			show_logo = true
+		}, 10)
+	})
+</script>
+{#if show_logo}
+	<img in:fly={{duration: 2500, y: 40}} src="/fp-logo.png" alt="">
+{/if}
