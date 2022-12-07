6ad63fe7ef90ee4aa5046fe33a76c6c46948ae91
cliff <cliff.wong@ehlacademy.org>
2022-12-07 10:31:37 +0800

    top bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../style/tailwind.css'
 </script>
 <div class="flex px-4 items-center border-b border-gray-400 h-12">
-	<p class="font-bold mr-4">Floating Project</p>
+	<a href="/" class="font-bold mr-4">Floating Project</a>
 	<a href="/blog">Art notes</a>
 	<a href="/event">Events</a>
 	<a href="/about">About</a>
