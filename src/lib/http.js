7cf7180542e7f15166f386d759e339468ac8408f
cliff <cliff.wong@ehlacademy.org>
2022-12-08 12:29:06 +0800

    env var

--- src/lib/http.js
+++ src/lib/http.js
@@ -1,3 +1,6 @@
+const VITE_API_BASE = import.meta.env.VITE_API_BASE_PROD
+const VITE_STRAPI_TOKEN = import.meta.env.VITE_STRAPI_TOKEN_PROD
+
 const onRes = async (res) => {
 	return await res.json()
 }
@@ -15,9 +18,9 @@ const getQueryUrl = (resource, query) => {
 const http = (() => {
 	async function get (fetch, resource, query) {
 		resource = getQueryUrl(resource, query)
-		const res = await fetch('http://localhost:1337/api' + resource, {
+		const res = await fetch(`${VITE_API_BASE}/api` + resource, {
 			headers: {
-				'Authorization': "bearer 8ad8e50b2b82202dcde9375fa93adef20b517576b410ebc5ab63b0ad7bb81465a78804a03fbb12425bdd736bd4fac5ac9cb2245077f477072cb6b56a8449c88a7ed67a4be4da41be82d21e39e4a3e88473326e5fcc9dbd029f396a388411e755005173a5a9c1312ae7900ca8adabe79b1b5860626246f53695319aa1080c6ae9"
+				'Authorization': `bearer ${VITE_STRAPI_TOKEN}`
 			}
 		})
 		return onRes(res)
@@ -25,11 +28,11 @@ const http = (() => {
 
 	// an empty object is necessary, otherwise result fatal error when not passing body params
 	async function post (fetch, resource, body = {}, config = {}) {
-		const res = await fetch('http://localhost:1337/api' + resource, {
+		const res = await fetch(`${VITE_API_BASE}/api` + resource, {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
-				'Authorization': "bearer 8ad8e50b2b82202dcde9375fa93adef20b517576b410ebc5ab63b0ad7bb81465a78804a03fbb12425bdd736bd4fac5ac9cb2245077f477072cb6b56a8449c88a7ed67a4be4da41be82d21e39e4a3e88473326e5fcc9dbd029f396a388411e755005173a5a9c1312ae7900ca8adabe79b1b5860626246f53695319aa1080c6ae9"
+				'Authorization': `bearer ${VITE_STRAPI_TOKEN}`
 			},
 			body: body && JSON.stringify(body)
 		})
