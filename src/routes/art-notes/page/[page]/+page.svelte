7fd1b39c27dce2eaf68cc86b38885e8dbcadf112
fukkuen <fukkuen@gmail.com>
2022-12-08 21:53:06 +0800

    ui

--- src/routes/art-notes/page/[page]/+page.svelte
+++ src/routes/art-notes/page/[page]/+page.svelte
@@ -2,23 +2,30 @@
 	export let data
 	import dayjs from 'dayjs'
 	import { base } from '$app/paths';
+	const VITE_IMAGE_BASE = import.meta.env.VITE_IMAGE_BASE
 	$: posts = data.data
 	let pagination = data.meta.pagination
 </script>
 
-<div class="p-4">
-	{#each posts as p}
-		<a href="{base}/art-notes/{p.id}" class="flex mb-8">
-			<span class="w-48 flex-shrink-0">
-				<img src="https://floatingprojectscollective.net/wp-content/uploads/{p.attributes.wp_thumb_url}" alt="">
-			</span>
-			<div class="ml-4">
-				<h2 class="text-xl"><b>{p.attributes.title}</b></h2>
-				<p class="mb-4 text-gray-700">{p.attributes.author.data.attributes.name}</p>
-				<p>{dayjs(p.attributes.createdAt).format('YYYY-MM-DD')}</p>
-				<div class="flex flex-wrap mt-2">
+<div class="container">
+	{#each posts as p, i}
+		<a href="{base}/art-notes/{p.id}" class="flex flex-col">
+			<div class="w-full sm:w-48 flex-shrink-0">
+				<img class="rounded-xl border border-gray-300 shadow-xl" src="{VITE_IMAGE_BASE}{p.attributes.wp_thumb_url}" alt="">
+			</div>
+			<div class="ml-0 sm:ml-4 mt-2 sm:mt-0">
+				<h2 class="text-lg leading-tight"><b>{p.attributes.title}</b></h2>
+				<div class="flex items-center">
+					<div class="w-8 h-8 rounded-full bg-gray-100 border border-gray-400"></div>
+					<div class="ml-2 my-2">
+						<p class="text-sm font-bold text-gray-700">{p.attributes.author.data.attributes.name}</p>
+						<p class="text-xs">發表於 {dayjs(p.attributes.createdAt).format('YYYY-MM-DD')}</p>
+					</div>
+				</div>
+				<p class="mb-2 text-gray-700 text-sm">{@html p.attributes.excerpt}</p>
+				<div class="flex flex-wrap">
 					{#each p.attributes.tags.data as t}
-						<p class="text-sm px-1 bg-gray-100 rounded border-gray-200 border mr-2 mb-1">{t.attributes.name}</p>
+						<p class="text-xs px-1 bg-gray-200 text-blue-900 italic rounded border-gray-200 border mr-2 mb-1">{t.attributes.name}</p>
 					{/each}
 				</div>
 				<div class="flex flex-wrap">
@@ -28,10 +35,16 @@
 				</div>
 			</div>
 		</a>
+		{#if i < posts.length - 1}
+			<div class="h-[1px] bg-gray-300 my-6"></div>
+		{/if}
 	{/each}
-	<div class="flex">
+</div>
+
+<div class="bg-gray-50 mt-6 py-6 border-t border-gray-300">
+	<div class="container flex flex-wrap justify-center">
 		{#each Array.from({length: pagination.pageCount}, (v, i) => i) as i}
-			<a class="flex justify-center items-center w-8 h-8 rounded-full bg-gray-100 border border-gray-300 mx-2" href="/art-notes/page/{i+1}">{i + 1}</a>
+			<a class="text-xs flex justify-center items-center w-8 h-8 rounded-full bg-gray-100 border border-gray-300 m-1" href="/art-notes/page/{i+1}">{i + 1}</a>
 		{/each}
 	</div>
 </div>
