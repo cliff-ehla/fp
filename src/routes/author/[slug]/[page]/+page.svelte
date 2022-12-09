17fdba9a51d3f38289ebd59bc1c916aa841e3eee
cliff <cliff.wong@ehlacademy.org>
2022-12-09 15:28:40 +0800

    fix pagination

--- src/routes/author/[slug]/[page]/+page.svelte
+++ src/routes/author/[slug]/[page]/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import PostPreview from "../../../../lib/PostPreview.svelte";
 	export let data
+	import {page} from "$app/stores";
 	$: posts = data.data
 	let pagination = data.meta.pagination
 </script>
@@ -18,7 +19,7 @@
 <div class="bg-gray-50 mt-6 py-6 border-t border-gray-300">
 	<div class="container flex flex-wrap justify-center">
 		{#each Array.from({length: pagination.pageCount}, (v, i) => i) as i}
-			<a class="text-xs flex justify-center items-center w-8 h-8 rounded-full bg-gray-100 border border-gray-300 m-1" href="/art-notes/page/{i+1}">{i + 1}</a>
+			<a class="text-xs flex justify-center items-center w-8 h-8 rounded-full bg-gray-100 border border-gray-300 m-1" href="/author/{$page.params.slug}/{i+1}">{i + 1}</a>
 		{/each}
 	</div>
 </div>
--- src/routes/tag/[slug]/[page]/+page.svelte
+++ src/routes/tag/[slug]/[page]/+page.svelte
@@ -18,7 +18,7 @@
 <div class="bg-gray-50 mt-6 py-6 border-t border-gray-300">
 	<div class="container flex flex-wrap justify-center">
 		{#each Array.from({length: pagination.pageCount}, (v, i) => i) as i}
-			<a class="text-xs flex justify-center items-center w-8 h-8 rounded-full bg-gray-100 border border-gray-300 m-1" href="/art-notes/page/{i+1}">{i + 1}</a>
+			<a class="text-xs flex justify-center items-center w-8 h-8 rounded-full bg-gray-100 border border-gray-300 m-1" href="/tag/page/{i+1}">{i + 1}</a>
 		{/each}
 	</div>
 </div>
