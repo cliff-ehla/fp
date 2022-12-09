9219a640111e15b87dfd0881e416086bee3a6d11
cliff <cliff.wong@ehlacademy.org>
2022-12-09 17:38:22 +0800

    category list update filter params

--- src/routes/art-notes/+layout.js
+++ src/routes/art-notes/+layout.js
@@ -1,8 +1,6 @@
 import http from "../../lib/http.js";
 export const load = async ({fetch, params}) => {
-	const category = await http.get(fetch, `/categories`, {
-		'filters[type][$eq]': 'Post'
-	})
+	const category = await http.get(fetch, `/categories`)
 	return {
 		category_list: category.data
 	}
