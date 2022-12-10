999755d1f03a720206a50c172df8b0f4aea19578
cliff <cliff.wong@ehlacademy.org>
2022-12-10 17:10:20 +0800

    event show relationsip

--- src/routes/events/[slug]/+page.svelte
+++ src/routes/events/[slug]/+page.svelte
@@ -2,6 +2,7 @@
 	export let data
 	let post = data.attributes
 	const VITE_IMAGE_BASE = import.meta.env.VITE_IMAGE_BASE
+	console.log(data)
 </script>
 
 <div class="post-body container leading-loose text-gray-700 text-lg">
@@ -23,5 +24,44 @@
 			<a href="/tag/{t.attributes.slug}/1"
 			   class="text-base bg-gray-50 px-2 py-1 mr-1 mb-1 rounded text-gray-600 border border-gray-400">{t.attributes.name}</a>
 		{/each}
+	</div>
+</div>
+
+<div class="container">
+	<h2 class="font-bold text-xl mb-4">Related reviews on this event</h2>
+	{#if post.reviews.data.length}
+		<div class="grid grid-cols-2 gap-8">
+			{#each post.reviews.data as post}
+				<a href="/art-notes/{post.attributes.slug}" class="border border-gray-400 rounded-lg overflow-hidden">
+					{#if post.attributes.image}
+						<img class="m-0 mb-4 rounded-xl shadow-lg w-full" src="{post.attributes.image.data.attributes.url}" alt={post.attributes.title}>
+					{:else}
+						<img src="{VITE_IMAGE_BASE}{post.attributes.wp_thumb_url}" alt={post.attributes.title}>
+					{/if}
+					<div class="p-2">
+						<h3 class="font-bold text-base leading-tight">{post.attributes.title}</h3>
+					</div>
+				</a>
+			{/each}
+		</div>
+	{:else}
+		<p>No data</p>
+	{/if}
+	<div class="my-8">
+		<h2 class="font-bold text-xl mb-4">Artists in this event</h2>
+		{#if post.artists.data.length}
+			<div class="grid grid-cols-3 gap-4">
+				{#each post.artists.data as artist}
+					<a href="/people/{artist.attributes.slug}" class="flex mb-4 items-center border border-gray-300 rounded-full">
+						<div class="flex-shrink-0 w-12 h-12 rounded-full bg-gray-100 border border-gray-400"></div>
+						<div class="px-2 leading-tight">
+							{artist.attributes.name}
+						</div>
+					</a>
+				{/each}
+			</div>
+		{:else}
+			No data
+		{/if}
 	</div>
 </div>
