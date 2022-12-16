0a4781fab31aeb948989b556722e1015410c8841
cliff <cliff.wong@ehlacademy.org>
2022-12-16 13:03:48 +0800

    event subtitle menu display

--- src/routes/events/+layout.svelte
+++ src/routes/events/+layout.svelte
@@ -6,7 +6,10 @@
 		{#each data.category_list as cat}
 			<a href="/events/category/{cat.attributes.slug}/1"
 			   class="text-sm py-0.5 mx-2 inline-block mb-0.5 whitespace-nowrap text-gray-700 border-b border-gray-400"
-			>{cat.attributes.name}</a>
+			>
+				<div>{cat.attributes.name}</div>
+				<div class="text-xs">{cat.attributes.subtitle}</div>
+			</a>
 		{/each}
 	</div>
 </div>
